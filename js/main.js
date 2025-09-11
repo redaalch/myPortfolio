cb85bd1c1c662df2b4e26e3f189fce474f44078b
redaalch <s133260695@taalim.ma>
2025-09-11 12:16:41 +0100

    Service Worker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 import "./modules/menu.js";
 import "./modules/todos.js";
 import "./modules/scroll.js";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").catch(function (err) {
+      // silent failure is fine
+      console.log("SW registration failed:", err);
+    });
+  });
+}
