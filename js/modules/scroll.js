fe83ac8546a4cd30e034e0887e63b51d270b0d6d
redaalch <s133260695@taalim.ma>
2025-09-19 00:36:16 +0100

    adding scroll

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -2,3 +2,6 @@ const header = document.querySelector('header');
 document.addEventListener('click', function (e) {
   const link = e.target.closest('a');
   if (!link) return;
+  const href = link.getAttribute('href');
+  if (!href) return;
+  if (href.charAt(0) !== '#') return;
